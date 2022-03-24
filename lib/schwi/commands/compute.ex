710a6e9165eddacd9208d8cc2a890b46f36261c1
marnixah <marnix@hagehoeve.com>
2022-03-24 15:23:20 +0100

    refactor(compute): use message lib

--- lib/schwi/commands/compute.ex
+++ lib/schwi/commands/compute.ex
@@ -1,28 +1,14 @@
 defmodule Schwi.Commands.Compute do
-  alias Nostrum.Api
-
   def run(msg, args) do
     query = Enum.join(args, " ")
     {result, response} = Schwi.Lib.GPT3.get_completion(query)
 
-    text =
-      case result do
-        :ok ->
-          Enum.random([
-            "Answer: #{response}",
-            "Response: #{response}"
-          ])
+    case result do
+      :ok ->
+        Schwi.Lib.Message.success(msg, response)
 
-        :error ->
-          Enum.random([
-            "Comprehension impossible: #{response}",
-            "Incomprehensible: #{response}"
-          ])
-      end
-
-    Api.create_message(msg.channel_id,
-      content: text,
-      message_reference: %{message_id: msg.id}
-    )
+      :error ->
+        Schwi.Lib.Message.error(msg, response)
+    end
   end
 end
