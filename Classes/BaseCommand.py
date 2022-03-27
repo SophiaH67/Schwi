ffe128bc7662172efb1cddba635d57795d3a50e6
marnixah <marnix@hagehoeve.com>
2022-03-28 01:04:22 +0200

    fix(BaseCommand): typehint schwi

--- Classes/BaseCommand.py
+++ Classes/BaseCommand.py
@@ -1,11 +1,15 @@
+import typing
 from Classes.MessageContext import MessageContext
 from abc import abstractmethod
+
+if typing.TYPE_CHECKING:
+  from Classes.Schwi import Schwi
 
 
 class BaseCommand:
   aliases = ["hello"]
 
-  def __init__(self, schwi):
+  def __init__(self, schwi: "Schwi"):
     self.schwi = schwi
 
   @abstractmethod
