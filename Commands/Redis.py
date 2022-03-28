7ce10715bf47a14a1ad30fc4c2a23727f40bd08a
marnixah <marnix@hagehoeve.com>
2022-03-28 14:35:26 +0200

    fix(Redis): stringify fallthrough

--- Commands/Redis.py
+++ Commands/Redis.py
@@ -8,9 +8,11 @@ class Command(BaseCommand):
     res = self.schwi.redis.execute_command(" ".join(ctx.args))
     if res is None:
       return "None"
+    print("res", res)
+    print("type(res)", type(res))
     if isinstance(res, bytes):
       return res.decode("utf-8")
     if isinstance(res, list):
       res = map(lambda x: x.decode("utf-8"), res)
       return "\n".join(res)
-    return res
+    return str(res)
