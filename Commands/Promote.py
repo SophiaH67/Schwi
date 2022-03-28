297977257c85eb39480f28feaf2f434f5a69aa3c
marnixah <marnix@hagehoeve.com>
2022-03-28 21:42:21 +0200

    feat: block, demote, promote commands

---
+++ Commands/Promote.py
@@ -0,0 +1,16 @@
+from Classes.BaseCommand import BaseCommand
+from Classes.Exceptions.PermissionsException import PermissionsException
+from Classes.MessageContext import MessageContext
+from Classes.PermissionsManager import PermissionLevel
+
+
+class Command(BaseCommand):
+  aliases = ["promote"]
+
+  async def run(self, ctx: MessageContext):
+    if ctx.permission_level != PermissionLevel.ADMIN:
+      raise PermissionsException()
+    if len(ctx.message.mentions) < 1:
+      raise Exception("You must mention a user")
+    user = ctx.message.mentions[0]
+    self.schwi.permissions_manager.set_permissions(user.id, PermissionLevel.ADMIN)
