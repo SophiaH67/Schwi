54b49b2a103e8daeb9e8115b3a9aef27ab35fe7b
marnixah <marnix@hagehoeve.com>
2022-04-30 17:45:02 +0200

    feat(commands): :sparkles: hello command

--- src/commands/Thanks.ts
+++ src/commands/Thanks.ts
@@ -2,7 +2,7 @@ import Command from "../../../eris-boreas/src/conversation/Command";
 import Conversation from "../../../eris-boreas/src/conversation/Conversation";
 import { info } from "../lib/transformer";
 
-export default class Test implements Command {
+export default class Thanks implements Command {
   public aliases = ["thank", "tskr"];
   public description = "Glad I could help";
   public usage = "Thanks!";
