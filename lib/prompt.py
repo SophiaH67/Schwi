4c7d540e8bba3be15e5d16f930020c818424eeff
marnixah <marnix@hagehoeve.com>
2022-07-17 13:19:46 +0200

    fix: prompt cleaning
    Clean mentions, emojis and caps

--- lib/prompt.py
+++ lib/prompt.py
@@ -1,5 +1,6 @@
 import logging
 from lib.minimum_permission_level import PermissionLevel
+from discord import Message
 
 
 class Prompt:
@@ -26,9 +27,24 @@ class Prompt:
         self.prompt += "\n"
         return self.prompt
 
-    async def add_chat_context(self, message, context):
+    async def add_chat_context(self, message, context: list[Message]):
         for message in context:
-            self.prompt += f"{message.author.name}: {message.content}\n"
+            content = message.content.replace("\n", " ")
+            # Replace all mentions with their name
+            for mention in message.mentions:
+                content = content.replace(f"<@{mention.id}>", mention.name.lower())
+                content = content.replace(f"<@!{mention.id}>", mention.name.lower())
+
+            # If a sticker is sent, add it to the prompt
+            for sticker in message.stickers:
+                content += f" {sticker.name}"
+            content = content.lower()
+
+            # Global replace of all double spaces with a single space
+            while "  " in content:
+                content = content.replace("  ", " ")
+
+            self.prompt += f"{message.author.name}: {content}\n"
         self.prompt += f"{self.schwi.user.name}:"
 
         return self.prompt
